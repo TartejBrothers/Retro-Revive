fe595837f738d05973c95772b45cf0fa3ffe906a
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-03-04 10:31:50 +0530

    Added : Accounts page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+# Import necessary modules from Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Import the User model from the current application
+from .models import User
+
+
+# Register the User model with the custom admin class
+admin.site.register(User)
--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,10 @@
+# Import the AppConfig class from the Django apps module
 from django.apps import AppConfig
 
-
+# Create an AppConfig for the 'accounts' app
 class AccountsConfig(AppConfig):
+    # Specify the default auto field for model creation
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Set the name of the app to 'accounts'
     name = 'accounts'
